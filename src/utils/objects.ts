6ccc0529e12f8d1dac72af7d576a957debcd533f
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-12 21:08:03 +0100

    chore: copy object utils these are utils i wrote for other personal projects so i just copied them across here

---
+++ src/utils/objects.ts
@@ -0,0 +1,7 @@
+export const getKeys = <O extends object>(object: O) =>
+  (typeof object === "object" ? Object.keys(object) : []) as (keyof O)[];
+
+export const mapKeys = <O extends object, R>(
+  object: O,
+  mapFn: (key: keyof O, value: O[keyof O], idx: number) => R
+) => getKeys(object).map((key, i) => mapFn(key, object[key], i));
