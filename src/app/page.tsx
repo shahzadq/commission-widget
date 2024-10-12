91aeafd9f7fc0e005f0ea711d5a0a85edf8966a5
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-12 21:39:13 +0100

    feat: calculate commission functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import { Widget } from "~/components/Widget";
+import {
+  calculateCommissionBreakdown,
+  calculateTotalCommission,
+} from "~/helpers/commission";
+
+const a = calculateCommissionBreakdown(18000);
+console.log({ breakdown: a, total: calculateTotalCommission(a) });
 
 export default function Home() {
   return (
---
+++ src/helpers/commission.ts
@@ -0,0 +1,26 @@
+import { ArrayElement } from "~/types";
+import { mapKeys } from "~/utils/objects";
+
+const commissionRateToBand = [0, 10, 15, 20, 25];
+const bandRange = 5000;
+
+export const calculateCommissionBreakdown = (revenue: number) =>
+  commissionRateToBand
+    .map((commissionRate) => {
+      // how much revenue is in the band
+      const revenueInBand = revenue >= bandRange ? bandRange : revenue;
+      // remove the amount from the total revenue for next cycle
+      revenue = revenue - revenueInBand;
+      // calculate the amount in commission and assign to the band
+      return { [commissionRate]: revenueInBand * (commissionRate / 100) };
+    })
+    .reduce((a, v) => ({ ...a, ...v }), {}) as Record<
+    ArrayElement<typeof commissionRateToBand>,
+    number
+  >;
+
+// calculate total commission by mapping over calculateCommissionBreakdown return
+// and adding together all the values
+export const calculateTotalCommission = (
+  breakdown: ReturnType<typeof calculateCommissionBreakdown>
+) => mapKeys(breakdown, (_, value) => value).reduce((a, v) => (a += v));
