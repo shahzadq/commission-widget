8693f521f8e52ba67d556ecc9cec5c3504cc852c
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 14:54:39 +0100

    fix: url base constant for prod

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,9 @@ export const media = {
   isNotMobile: mq({ type: "screen", minWidth: 700 }),
 } as const;
 
+// in production use the vercel auto generated url base
+// https://vercel.com/docs/projects/environment-variables/framework-environment-variables
 export const urlBase =
   process.env.NODE_ENV === "development"
     ? ("http://localhost:3000" as const)
-    : ("https://something.vercel.domain" as const);
+    : (`https://${process.env.NEXT_PUBLIC_VERCEL_URL}` as const);
