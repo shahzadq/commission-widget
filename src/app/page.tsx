6be40829cf3236c314d70daf75793541df946535
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 11:16:58 +0100

    feat: add page styled wrapper to center content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import styled from "styled-components";
 import { CommissionWidget } from "~/components/CommissionWidget";
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function Home() {
   return (
-    <div>
+    <StyledWrapper>
       <CommissionWidget />
-    </div>
+    </StyledWrapper>
   );
 }
