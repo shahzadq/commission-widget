5ae1a1f65a04bd2181b049c94f54b1473849a377
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 11:34:24 +0100

    feat: style commission widget form

--- src/components/CommissionWidget.tsx
+++ src/components/CommissionWidget.tsx
@@ -4,6 +4,52 @@ import { type FormEvent, useState } from "react";
 import { usePostCalculateCommission } from "~/hooks/usePostCalculateCommission";
 import { revenueSchema } from "~/schemas/commission";
 import { Widget, WidgetTitle } from "./Widgets";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledLabel = styled.label`
+  font-size: small;
+  color: rgba(0, 0, 0, 0.75);
+  margin-bottom: 2px;
+`;
+
+const StyledInputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: var(--border-regular);
+  padding: 4px 8px;
+  border-radius: 4px;
+  gap: 4px;
+  transition: all ease-in-out 0.2s;
+
+  &:focus-within {
+    border-color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const StyledPreInput = styled.div`
+  color: rgba(0, 0, 0, 0.75);
+  font-weight: bold;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  border: none;
+  font-weight: bold;
+
+  &:focus-visible {
+    outline: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  margin-top: 8px;
+`;
 
 export const CommissionWidget = () => {
   const [inputs, setInputs] = useState({ revenue: "" });
@@ -21,16 +67,20 @@ export const CommissionWidget = () => {
   return (
     <Widget>
       <WidgetTitle>Commission Estimate</WidgetTitle>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="number"
-          value={inputs.revenue}
-          onChange={(e) => {
-            setInputs((inputs) => ({ ...inputs, revenue: e.target.value }));
-          }}
-        />
-        <button type="submit">Calculate Commission</button>
-      </form>
+      <StyledForm onSubmit={handleFormSubmit}>
+        <StyledLabel>Revenue Estimate</StyledLabel>
+        <StyledInputWrapper>
+          <StyledPreInput>£</StyledPreInput>
+          <StyledInput
+            type="number"
+            value={inputs.revenue}
+            onChange={(e) => {
+              setInputs((inputs) => ({ ...inputs, revenue: e.target.value }));
+            }}
+          />
+        </StyledInputWrapper>
+        <StyledButton type="submit">Calculate Commission</StyledButton>
+      </StyledForm>
       <div>
         {isLoading && "loading..."}
         {error && error}
