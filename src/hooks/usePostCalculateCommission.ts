188ab0b7fa3540c34d5dfd5bb7d4647e0683ec77
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 10:01:50 +0100

    feat: add generic state to hook

--- src/hooks/usePostCalculateCommission.ts
+++ src/hooks/usePostCalculateCommission.ts
@@ -1,5 +1,14 @@
+"use client";
+
 // hook to implement api fetch and parsing whatever response comes back
 
+import { useState } from "react";
+import type { calculateCommission } from "~/helpers/commission";
+
 export const usePostCalculateCommission = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string>();
+  const [data, setData] = useState<ReturnType<typeof calculateCommission>>();
+
   return {};
 };
