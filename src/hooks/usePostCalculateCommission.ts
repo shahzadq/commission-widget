2f9db0753a0ae23ce8e263c2356fb7a200a637a0
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 10:14:46 +0100

    feat: implement fetch in hook

--- src/hooks/usePostCalculateCommission.ts
+++ src/hooks/usePostCalculateCommission.ts
@@ -2,13 +2,44 @@
 
 // hook to implement api fetch and parsing whatever response comes back
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { calculateCommission } from "~/helpers/commission";
 
-export const usePostCalculateCommission = () => {
+export const usePostCalculateCommission = (revenue?: number) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string>();
   const [data, setData] = useState<ReturnType<typeof calculateCommission>>();
 
-  return {};
+  // we need this function to be seperate from useEffect becuase you cant have an async useeffect callback
+  const handleFetch = async () => {
+    setIsLoading(true);
+
+    try {
+      const res = await fetch("/api/calculate-commission", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ revenue }),
+      });
+
+      const data = await res.json();
+
+      if (data.type === "error") setError(data.message);
+      else setData(data.commission);
+    } catch {
+      setError("Something went wrong. Try reloading this page.");
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (typeof revenue !== "undefined") {
+      handleFetch();
+    }
+  }, [revenue]);
+
+  return { isLoading, error, data };
 };
