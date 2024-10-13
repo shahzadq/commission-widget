c5509dd49483966eac660f96d449573428fad592
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-13 09:56:40 +0100

    feat: create basic form component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
 export const Widget = () => {
-  return <div>widget</div>;
+  const [revenue, setRevenue] = useState("");
+
+  return (
+    <div>
+      <form>
+        <input
+          type="number"
+          value={revenue}
+          onChange={(e) => {
+            setRevenue(e.target.value);
+          }}
+        />
+        <button type="submit">Calculate Commission</button>
+      </form>
+    </div>
+  );
 };
